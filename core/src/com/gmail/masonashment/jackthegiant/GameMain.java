8627e4e87ff2a15eb63633b9d9ec9ced6bdfd591
Mason Ashment <masonashment@gmail.com>
2019-07-28 21:21:20 -0600

    beginning architecture, drawing backgrounds, viewports

--- core/src/com/gmail/masonashment/jackthegiant/GameMain.java
+++ core/src/com/gmail/masonashment/jackthegiant/GameMain.java
@@ -7,28 +7,30 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import scenes.Gameplay;
+
 public class GameMain extends Game {
 	private SpriteBatch batch;
-	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("Backgrounds/Game BG.png");
+
+		setScreen(new Gameplay(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
+
+	}
+
+	public SpriteBatch getBatch() {
+		return this.batch;
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
---
+++ core/src/scenes/Gameplay.java
@@ -0,0 +1,145 @@
+package scenes;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.gmail.masonashment.jackthegiant.GameMain;
+
+import helpers.GameInfo;
+
+public class Gameplay implements Screen {
+
+    private GameMain game;
+
+    private OrthographicCamera mainCamera;
+    private Viewport gameViewport;
+
+    private Sprite[] bgs;
+
+    public Gameplay(GameMain game) {
+        this.game = game;
+
+        mainCamera = new OrthographicCamera(GameInfo.WIDTH, GameInfo.HEIGHT);
+        mainCamera.position.set(GameInfo.WIDTH/2f, GameInfo.HEIGHT/2f, 0);
+
+        gameViewport = new StretchViewport(GameInfo.WIDTH, GameInfo.HEIGHT);
+
+        createBackgrounds();
+    }
+
+    public void update(float dt) {
+        moveCamera();
+    }
+
+    @Override
+    public void render(float delta) {
+        update(delta);
+
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.getBatch().begin();
+
+        drawBackgrounds();
+
+        game.getBatch().end();
+
+        game.getBatch().setProjectionMatrix(mainCamera.combined);
+        mainCamera.update();
+    }
+
+    public void moveCamera() {
+        mainCamera.position.y -= 1;
+    }
+
+    public void createBackgrounds() {
+        bgs = new Sprite[3];
+
+        for (int i = 0; i < bgs.length; i++) {
+            bgs[i] = new Sprite(new Texture("Backgrounds/Game BG.png"));
+            bgs[i].setPosition(0, -(i * bgs[i].getHeight()));
+        }
+    }
+
+    public void drawBackgrounds() {
+        for (int i = 0; i < bgs.length; i++) {
+            game.getBatch().draw(bgs[i], bgs[i].getX(),bgs[i].getY());
+
+        }
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+} // gameplay
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
